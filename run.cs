9964eb915c0984383a272aa58a29a81dfb40d7da
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-09-15 12:05:14 +0700

    a

---
+++ run.cs
@@ -0,0 +1,34 @@
+﻿using System; 
+
+namespace Fleck2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new WebSocketServer("ws://localhost:11111");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => Console.WriteLine("Open!");
+                socket.OnClose = () => Console.WriteLine("Close!");
+                socket.OnMessage = message => Console.WriteLine(message + Environment.NewLine);
+                //socket.Send(message);
+            });
+             
+            Console.Title = "ws|http://+:11111/";
+            string cm = Console.ReadLine();
+            while (true)
+            {
+                switch (cm)
+                {
+                    case "clear":
+                    case "cls":
+                        Console.Clear();
+                        break;
+                }
+                cm = Console.ReadLine();
+            }
+        }
+    }
+     
+}
